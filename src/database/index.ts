f097fdd2bf53dd8370081558a67b7862076bfe0c
Juan Victor Leite <jnvleite@gmail.com>
2023-04-25 20:35:50 -0300

    add migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -11,5 +11,13 @@ export const AppDataSource = new DataSource({
   logging: true,
   entities: [],
   subscribers: [],
-  migrations: [],
+  migrations: ["src/database/migrations/*.ts"],
 });
+
+// AppDataSource.initialize()
+//   .then(async () => {
+//     // do anything here like connecting to your express server or adding document to your db
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
